b810ad8f89cc98710312912a0b9c7539e6b48266
Harshshen <98032169+Harshshen@users.noreply.github.com>
2023-12-23 20:05:42 +0530

    h

--- contractAddress.js
+++ contractAddress.js
@@ -1,5 +1,5 @@
 
     module.exports = {
-      nftAddress : "0x28293f72C1f5ED63f8c285605642C9ed2e6f5235"
+      nftAddress : "0x47281960917533282FAd3D4A1fc27ababAF514b1"
     }
   
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,6 @@
 require("@nomiclabs/hardhat-waffle");
 require("dotenv").config();
+require("@nomicfoundation/hardhat-verify");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -13,5 +14,19 @@ module.exports = {
       url: 'https://ethereum-goerli.publicnode.com',
       accounts: [process.env.PRIVATE_KEY],
     },
+    sepolia: {
+      url: 'https://ethereum-sepolia.publicnode.com',
+      apikey: 'https://eth-sepolia.g.alchemy.com/v2/[MYALCHEMY_API_KEY]',
+      accounts: [process.env.PRIVATE_KEY],
+      etherscan: {
+        // Your API key for Etherscan
+        // Obtain one at https://etherscan.io/
+        
+        apiKey: '1N81UN57Q3KG2F358G643NW9RR2CZ1GAAX',
+      },
+      
+    },
+    
   }
 };
+
